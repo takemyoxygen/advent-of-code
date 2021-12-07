225ae3acf2ca49cbd456d9d7161815493cae1b3e
Uladzimir Makarau <uladzimir.makarau@gmail.com>
2021-12-07 09:23:19 +0300

    2018 - day 4 part 2

--- 2018/day4.py
+++ 2018/day4.py
@@ -5,52 +5,55 @@ import core
 from collections import defaultdict
 
 
-# falls asleep | wakes up | (begins shift, 123)
-GuardAction: TypeAlias = str | tuple[str, int]
-GuardEvent: TypeAlias = tuple[datetime, GuardAction]
+GuardEvent: TypeAlias = tuple[datetime, str]
 
-
-def get_action(action_string: str) -> GuardAction:
-    match action_string:
-        case "falls asleep" | "wakes up":
-            return action_string
-        case _:
-            return ("begins shift", int(re.match('Guard #(\d+) begins shift', action_string).groups()[0]))
+# guard ID -> list of minutes asleep per minute 0-59
+SleepMatrix: TypeAlias = dict[int, list[int]]
 
 
 def parse_input(lines: list[str]) -> list[GuardEvent]:
     pattern = "^\[(.+)\] (.*)"
     return [
-        (datetime.strptime(timestamp, '%Y-%m-%d %H:%M'), get_action(action))
+        (datetime.strptime(timestamp, '%Y-%m-%d %H:%M'), action)
         for timestamp, action in (re.match(pattern, line).groups() for line in lines)
     ]
 
 
-def part1(events: list[GuardEvent]) -> int:
-    events = sorted(events, key=lambda evt: evt[0])
-    sleeps: dict[int, list[tuple[int, int]]] = defaultdict(list)
+def get_sleep_minutes_matrix(lines: list[str]) -> SleepMatrix:
+    events = sorted(parse_input(lines), key= lambda evt: evt[0])
+    sleep_matrix = defaultdict(lambda: [0] * 60)
     guard = None
     started_sleeping = None
 
     for timestamp,action in events:
         match action:
-            case ("begins shift", id):
-                guard = id
             case "falls asleep":
                 started_sleeping = timestamp.minute
             case "wakes up":
-                sleeps[guard].append((started_sleeping, timestamp.minute))
+                for minute in range(started_sleeping, timestamp.minute):
+                    sleep_matrix[guard][minute] += 1
                 started_sleeping = None
+            case _:
+                guard = int(re.match('Guard #(\d+) begins shift', action).groups()[0])
 
-    target_guard = max(sleeps, key=lambda id: sum(end - start for start, end in sleeps[id]))
-    sleep_minutes = defaultdict(lambda: 0)
-    for start,end in sleeps[target_guard]:
-        for minute in range(start, end):
-            sleep_minutes[minute] += 1
+    return sleep_matrix
 
-    max_minute = max(sleep_minutes, key=sleep_minutes.get)
 
+def part1(sleep_matrix: SleepMatrix) -> int:
+    target_guard = max(sleep_matrix, key=lambda guard: sum(sleep_matrix[guard]))
+    max_minute = max(range(0, 60), key=lambda minute: sleep_matrix[target_guard][minute])
     return target_guard * max_minute
 
 
-core.run(part1, process_input=parse_input)
+def part2(sleep_matrix: SleepMatrix) -> int:
+    result = 0
+    max_slept = 0
+    for guard in sleep_matrix:
+        for minute in range(len(sleep_matrix[guard])):
+            if sleep_matrix[guard][minute] > max_slept:
+                max_slept = sleep_matrix[guard][minute]
+                result = guard * minute
+    return result
+
+
+core.run(part1, part2, process_input=get_sleep_minutes_matrix)
