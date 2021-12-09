950ba88057c92f1daae1d3dcb17102df2d318ae2
Uladzimir Makarau <uladzimir.makarau@gmail.com>
2021-12-09 19:03:53 +0300

    2018 - day 7

--- 2018/day7.py
+++ 2018/day7.py
@@ -1,7 +1,7 @@
 import re
 from core import run, fst
-from collections import defaultdict, deque
-from bisect import bisect_right
+from collections import defaultdict
+import heapq
 
 def parse_input(lines: list[str], options) -> tuple[dict[str, set[str]], set[str], int, int]:
     pattern = '^Step (.) must be finished before step (.) can begin\.$'
@@ -28,53 +28,54 @@ def complete_job(job: str, deps: dict[str, set[str]], remaining: set[str]) -> No
     remaining.remove(job)
 
 
+def clone_deps(deps_map: dict[str, set[str]]) -> dict[str, set[str]]:
+    copy = deps_map.copy()
+    for job in copy:
+        copy[job] = copy[job].copy()
+    return copy
+
+
 def part1(deps_map: dict[str, set[str]], all_jobs: set[str], _: int, __:int) -> str:
     remaining_jobs = all_jobs.copy()
-    deps_map = deps_map.copy()
+    deps_map = clone_deps(deps_map)
     result = ''
 
     while remaining_jobs:
         next_job = fst(get_next_jobs(deps_map, remaining_jobs))
         complete_job(next_job, deps_map, remaining_jobs)
         result += next_job
-    
+
     return result
 
 
 def part2(deps_map: dict[str, set[str]], all_jobs: set[str], workers: int, extra_duration: int) -> str:
     remaining_jobs = all_jobs.copy()
-    deps_map = deps_map.copy()
+    deps_map = clone_deps(deps_map)
     scheduled_jobs = set()
-    result = ''
 
     times_queue = [0]
     job_completion_times = defaultdict(list)
     free_workers = workers
 
+    time = 0
+
     while remaining_jobs:
-        time = times_queue.pop(0)
-        print('Time: ', time)
-        # print('Remaining jobs', remaining_jobs)
-        # print('Job completion', job_completion_times)
+        time = heapq.heappop(times_queue)
 
         for to_complete in job_completion_times[time]:
             complete_job(to_complete, deps_map, remaining_jobs)
             scheduled_jobs.remove(to_complete)
-            print('Completing job', to_complete)
             free_workers += 1
-            result += to_complete
 
         available_jobs = [job for job in get_next_jobs(deps_map, remaining_jobs) if job not in scheduled_jobs][:free_workers]
         for job in available_jobs:
-            print('Scheduling job', job)
             completion_time = time + extra_duration + (ord(job) - ord('A') + 1)
             job_completion_times[completion_time].append(job)
             free_workers -= 1
-            time_index = bisect_right(times_queue, completion_time)
-            times_queue.insert(time_index, completion_time)
+            heapq.heappush(times_queue, completion_time)
             scheduled_jobs.add(job)
 
-    return result
+    return time
 
 
 
