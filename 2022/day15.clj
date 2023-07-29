1ee7ad29fdcbd959d10b6392f36d280355d5c464
takemyoxygen <uladzimir.makarau@gmail.com>
2023-07-29 17:43:02 +0100

    2022 - day 15 part 2

--- 2022/day15.clj
+++ 2022/day15.clj
@@ -27,7 +27,7 @@
        :radius (dist sensor beacon)}))))
 
 
-(defn get-intervals [base items]
+(defn get-intervals-x [base items]
   (keep 
     (fn [{rad :radius, [x y] :sensor}] 
       (let [vertical-dist (abs (- y base))
@@ -36,10 +36,19 @@
     items))
 
 
+(defn get-intervals-y [base items]
+  (keep 
+    (fn [{rad :radius, [x y] :sensor}] 
+      (let [horizontal-dist (abs (- x base))
+            dy (- rad horizontal-dist)]
+        (if (> 0 dy) nil [(- y dy) (+ y dy)])))
+    items))
+
+
 (defn part1 [items]
   (let [base      2000000
         beacons   (set (map :beacon items))
-        intervals (sort (get-intervals base items))
+        intervals (sort (get-intervals-x base items))
         at-base   (count (filter #(= base (% 1)) beacons))
         [points]    (reduce
                       (fn [[cnt prev-end] [start end]]
@@ -51,3 +60,24 @@
                       intervals)]
     (- points at-base)))
 
+(defn part2 [items]
+  (let [limit     4000000
+        xs        (range limit)
+        [x y]     (->> xs
+                    (keep
+                      (fn [x]
+                        (loop [intervals    (vec (sort (get-intervals-y x items)))
+                               candidate-y  0]
+                          (if (empty? intervals) nil
+                            (let [[[start end] & rest] intervals
+                                  ]
+                              (if
+                                (< candidate-y start) [x candidate-y]
+                                (recur rest (max (inc end) candidate-y))))))))
+                    (first))]
+    (+ y (* 4000000 x))))
+
+
+(println "Day 15")
+(println "Part 1:" (part1 items))
+(println "Part 2:" (part2 items))
