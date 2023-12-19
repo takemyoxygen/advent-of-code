a2a17fa934a937fd3221e933b1c47df07c7decac
takemyoxygen <uladzimir.makarau@gmail.com>
2023-12-19 09:59:33 +0000

    2023 - day 13

---
+++ 2023/day13.fsx
@@ -0,0 +1,84 @@
+#load "utils.fsx"
+
+open Utils
+
+type ReflectionLine =
+  | Col of int
+  | Row of int
+
+let lines = Input.readLines 13 false
+
+let input =
+  let readGrid lines =
+    let rec loop acc lines =
+      match lines with
+      | [] -> acc, []
+      | "" :: rest -> acc, rest
+      | line :: rest -> loop (line :: acc) rest
+
+    let result, rest = loop List.empty lines
+
+    result |> Seq.rev |> Seq.map _.ToCharArray() |> Array.ofSeq |> Grid.create,
+    rest
+
+  Seq.unfold
+    (function
+    | [] -> None
+    | lines -> Some(readGrid lines))
+    (List.ofArray lines)
+  |> List.ofSeq
+
+let findReflections (items: seq<'a>) =
+  let hashes = items |> Seq.map _.GetHashCode() |> Array.ofSeq
+
+  let isMirror idx =
+    let toCheck = min idx (hashes.Length - idx)
+
+    seq { 0 .. toCheck - 1 }
+    |> Seq.all (fun offset -> hashes[idx + offset] = hashes[idx - offset - 1])
+
+  seq { 1 .. hashes.Length - 1 } |> Seq.filter isMirror
+
+let columnReflections grid =
+  grid |> Grid.columns |> findReflections |> Seq.map Col
+
+let rowReflections grid =
+  grid |> Grid.rows |> findReflections |> Seq.map Row
+
+let findReflectionLines grid =
+  Seq.append (columnReflections grid) (rowReflections grid)
+
+let score =
+  function
+  | Col x -> x
+  | Row x -> x * 100
+
+let part1 () =
+  input |> Seq.collect findReflectionLines |> Seq.map score |> Seq.sum
+
+let reflectionWithSmudge g =
+  let originalReflection = findReflectionLines g |> Seq.head
+
+  seq {
+    for rowIdx in seq { 0 .. Grid.rowsCount g - 1 } do
+      for colIdx in seq { 0 .. Grid.columnsCount g - 1 } do
+        yield
+          Grid.updateWith
+            rowIdx
+            colIdx
+            (function
+            | '.' -> '#'
+            | _ -> '.')
+            g
+  }
+  |> Seq.collect findReflectionLines
+  |> Seq.find ((<>) originalReflection)
+
+
+let part2 () =
+  input |> Seq.map reflectionWithSmudge |> Seq.map score |> Seq.sum
+
+
+printfn "Day 13"
+printfn "Part 1: %A" <| part1 ()
+printfn "Part 2: %A" <| part2 ()
--- 2023/utils.fsx
+++ 2023/utils.fsx
@@ -23,3 +23,31 @@ module String =
 module List =
   let repeat n xs =
     xs |> List.replicate n |> List.collect id
+
+
+type Grid<'a> = Grid of array<array<'a>>
+
+module Grid =
+  let create items = Grid items
+
+  let column colIdx (Grid content) =
+    seq { 0 .. content.Length - 1 }
+    |> Seq.map (fun rowIdx -> content[rowIdx][colIdx])
+    |> List.ofSeq
+
+  let row rowIdx (Grid content) = content[rowIdx] |> List.ofArray
+
+  let columnsCount (Grid content) = content[0].Length
+
+  let rowsCount (Grid content) = content.Length
+
+  let columns grid =
+    seq { 0 .. (columnsCount grid) - 1 } |> Seq.map (fun idx -> column idx grid)
+
+  let rows (Grid content) = content |> Seq.map (List.ofArray)
+
+  let updateWith row column update (Grid content) =
+    let newRow =
+      Array.updateAt column (update <| content[row][column]) content[row]
+
+    content |> Array.updateAt row newRow |> create
